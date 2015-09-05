c6872e954c7635095e9b94d8895e7d4d4d4bb61c
Arif Sundrani <arifsundrani@gmail.com>
2015-09-05 14:50:48 -0700

    Minor updates

--- HighlightText/highlight.js
+++ HighlightText/highlight.js
@@ -1,7 +1,9 @@
 var area = window.getSelection().getRangeAt(0);
+var location = area.getBoundingClientRect();
 var selection = area.extractContents();
 var selText = document.createElement("selText");
 selText.appendChild(selection);
 selText.setAttribute("class","uiWebviewHighlight");
 selText.style.backgroundColor = "yellow";
+selText = selText.fontcolor("blue");
 area.insertNode(selText);
